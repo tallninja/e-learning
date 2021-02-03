1aa621b8558dfd4eff00415a31cb2e2696ebc6af
tallninja <tallninja123@gmail.com>
2021-02-03 21:51:14 +0300

    second commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("config");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
+
+// mongo
+const mongoURI = config.get("mongo.URI");
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
-app.get("/", (req, res) => res.send("welcome"));
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
+// sesions and cookies
+app.use(
+  cookieSession({
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    keys: [config.get("cookieSession.key")],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(PORT, () => console.log("Server listening on port " + PORT));
+// auth
+const authRoutes = require("./routes/authRoutes");
+app.use("/auth", authRoutes);
+
+// api
+const blogRoutes = require("./routes/");
+app.use("/api", blogRoutes);
+
+app.listen(5000, () => {
+  console.log("Server started successfully !");
+});
