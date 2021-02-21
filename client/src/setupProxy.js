9c46008aed534283ecc599678c76a9b86a5a7877
tallninja <tallninja123@gmail.com>
2021-02-21 15:04:40 +0300

    hopefully this works

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -8,6 +8,14 @@ module.exports = (app) => {
         target: "http://e-learning_server_1:5000",
       })
     );
+  } else if (process.env.NODE_ENV === "production") {
+    const PORT = process.env.PORT || 5000;
+    app.use(
+      ["/auth", "/api"],
+      createProxyMiddleware({
+        target: `https://dry-shore-76914.herokuapp.com:${PORT}`,
+      })
+    );
   } else {
     app.use(
       ["/auth", "/api"],
