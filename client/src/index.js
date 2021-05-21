81e59ad480687d02d047d677d1a758c664874c1c
tallninja <tallninja123@gmail.com>
2021-05-21 12:52:37 +0300

    removed redux dev tools in production environment

--- client/src/index.js
+++ client/src/index.js
@@ -7,15 +7,26 @@ import thunk from "redux-thunk";
 import reducers from "./reducers";
 import App from "./components/App";
 
-const composeEnhancers =
-  (typeof window !== "undefined" &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+if (process.env.NODE_ENV === "production") {
+  const store = createStore(reducers, applyMiddleware(thunk));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
-);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.querySelector("#root")
+  );
+} else {
+  const composeEnhancers =
+    (typeof window !== "undefined" &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+  const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.querySelector("#root")
+  );
+}
