948ec52324b82c576dba1fe72568082a047dd210
tallninja <tallninja123@gmail.com>
2021-06-01 20:13:52 +0300

    added toggle password visibility

--- client/src/components/login/LoginField.js
+++ client/src/components/login/LoginField.js
@@ -1,16 +1,53 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class LoginField extends Component {
-  render() {
+  state = { password: true };
+
+  renderField = () => {
     const { name, placeholder, type, icon, input } = this.props;
-    return (
-      <div className="field">
-        <div className="ui left icon input">
-          <i className={`${icon} icon`}></i>
-          <input name={name} placeholder={placeholder} type={type} {...input} />
+    if (type === "password") {
+      return (
+        <div className="field">
+          <div className="ui left icon action input">
+            <i className="key icon"></i>
+            <input
+              name={name}
+              placeholder={placeholder}
+              type={this.state.password ? "password" : "text"}
+              {...input}
+            />
+            <div
+              className="ui icon button"
+              onClick={() => this.setState({ password: !this.state.password })}
+            >
+              <i
+                className={`eye ${
+                  this.state.password ? "" : "slash"
+                } large icon`}
+              ></i>
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="field">
+          <div className="ui left icon input">
+            <i className={`${icon} icon`}></i>
+            <input
+              name={name}
+              placeholder={placeholder}
+              type={type}
+              {...input}
+            />
+          </div>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    return <Fragment>{this.renderField()}</Fragment>;
   }
 }
 
